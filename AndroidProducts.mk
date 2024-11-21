cd88787ffa1cc655f859fedab0bfe1f864e2ca24
CaullenOmdahl <77470883+CaullenOmdahl@users.noreply.github.com>
2024-11-21 15:23:58 +0700

    reverted last commit because of my bird brain typo

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The OmniRom Project
+# Copyright (C) 2021 The twrpRom Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_klein.mk
+    $(LOCAL_DIR)/twrp_klein.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_klein-user \
-    omni_klein-userdebug \
-    omni_klein-eng
+    twrp_klein-user \
+    twrp_klein-userdebug \
+    twrp_klein-eng
